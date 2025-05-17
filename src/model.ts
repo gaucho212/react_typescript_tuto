824bb1b6ed5a43b5a8fbb568b91e733208d87009
gaucho212 <adamwoz2005@gmail.com>
2025-05-17 19:18:09 +0200

    Projekt

---
+++ src/model.ts
@@ -0,0 +1,12 @@
+
+export interface Todo {
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+
+export type Actions =
+  | { type: "add"; payload: string }
+  | { type: "remove"; payload: number }
+  | { type: "done"; payload: number }
+  | { type: "edit"; payload: { id: number; todo: string } };
